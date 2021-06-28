6ce221ce17eacb34d828ab045e17d0a97e6525bf
gamesfreak26 <gamesfreak26@gmail.com>
2021-06-28 13:20:26 +1000

    Create new and edit forms

--- app/controllers/coffee_controller.rb
+++ app/controllers/coffee_controller.rb
@@ -1,15 +1,7 @@
 class CoffeeController < ApplicationController
 
-    skip_before_action :verify_authenticity_token
-
-    before_action :set_coffee, only: [:show, :destroy]
-
-    # pretend model
-    # @@coffees = [
-    #     {id: 1, name: "Latte", description: "Milk iin coffee with foam on top", price: 4},
-    #     {id: 2, name: "Affogato", description: "Coffee with ice cream", price: 15},
-    #     {id: 3, name: "Americano", description: "Black coffee", price: 1}
-    # ]
+    before_action :coffee_params, only: [:create, :update]
+    before_action :set_coffee, only: [:edit, :show, :destroy]
 
     def index
         @coffees = Coffee.all
@@ -19,42 +11,21 @@ class CoffeeController < ApplicationController
         
     end
 
+    def new
+    end
+
     def create
+        @coffee = Coffee.create(coffee_params)
+        redirect_to coffee_path
+    end
 
-        created_coffee = {
-            id: @@coffees.last[:id] + 1,
-            name: params[:name],
-            description: params[:description],
-            price: params[:price]
-        }
+    def edit
         
-        @@coffees << created_coffee
-
-        render json: created_coffee, status: 200
-
     end
 
     def update
-
-        if @index 
-            ## Pull existing coffee
-            @coffee = @@coffees[@index]
-
-            ## update coffee
-            updated_coffee = {
-                id: @coffee[:id],
-                name: params[:name],
-                description: params[:description],
-                price: params[:price]
-            }
-    
-            @@coffees[@index] = updated_coffee
-            render json: updated_coffee, status: 200
-        else
-            ## 404
-            render json: { error: "Could not find coffee"}, status: 404
-        end
-
+        @coffee.update(coffee_params)
+        redirect_to show_coffee_path
     end
 
     def destroy
@@ -71,6 +42,10 @@ class CoffeeController < ApplicationController
     end
 
     private 
+
+    def coffee_params
+        params.require(:coffee).permit(:name, :description, :price)
+    end
 
     def set_coffee
         @coffee = Coffee.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :coffee, only: [:show]
-  
   root to: "pages#index" # homepage
   get "/coffee", to: "coffee#index"
-  post "/coffee", to: "coffee#create"
+  post "/coffee", to: "coffee#create", as: "create_coffee"
+  get "/coffee/new", to: "coffee#new"
+  get "/coffee/:id/edit", to: "coffee#edit", as: "edit_coffee"  
 
-  get "/coffee/:id", to: "coffee#show"
-  put "/coffee/:id", to: "coffee#update"
+  get "/coffee/:id", to: "coffee#show", as: "show_coffee"
+  put "/coffee/:id", to: "coffee#update", as: "update_coffee"
   delete "/coffee/:id", to: "coffee#destroy"
 
   # get "/contact", to: "coffee#contact"
